938b6c5593e4417dd8bdb7809635f03fde47c777
norman-schmidt <60552466+norman-schmidt@users.noreply.github.com>
2020-05-28 18:34:17 +0200

    Carousel functionality

--- cvsite/views.py
+++ cvsite/views.py
@@ -3,8 +3,16 @@ from django.shortcuts import render
 # Create your views here.
 #home page
 def get_home(request):
-    return render(request, 'home.html', {'page_title' : 'Home CV Generator'})
+    return render(request, 'home.html', {'page_title': 'Home CV Generator'})
 
 def get_cv_generator(request):
-    edu = ['template1_education.html', 'template1_education.html', 'template1_education.html']
-    return render(request, 'generate_cv.html', {'page_title' : 'Generate your CV', 'edu' : edu})
+    ids = ['Photo', 'Name', 'Adress', 'Skills', 'About', 'Experience', 'Education']
+    templates = {"Photo": ['template1_photo.html', 'template1_photo.html', 'template1_photo.html'],
+                 "Name": ['template1_name.html', 'template1_name.html', 'template1_name.html'],
+                 "Adress": ['template1_adress.html', 'template1_adress.html', 'template1_adress.html'],
+                 "Skills": ['template1_skills.html', 'template1_skills.html', 'template1_skills.html'],
+                 "About": ['template1_about.html', 'template1_about.html', 'template1_about.html'],
+                 "Experience": ['template1_experience.html', 'template1_experience.html', 'template1_experience.html'],
+                 "Education": ['template1_education.html', 'template1_education.html', 'template1_education.html']
+                 }
+    return render(request, 'generate_cv.html', {'page_title': 'Generate your CV', 'ids': ids, 'templates': templates})
