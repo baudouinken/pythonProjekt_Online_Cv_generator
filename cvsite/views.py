8108b147cb0233f0c885538abae8d00a5fda6d7f
norman-schmidt <60552466+norman-schmidt@users.noreply.github.com>
2020-05-28 17:23:33 +0200

    Carousel functionality

--- cvsite/views.py
+++ cvsite/views.py
@@ -6,4 +6,5 @@ def get_home(request):
     return render(request, 'home.html', {'page_title' : 'Home CV Generator'})
 
 def get_cv_generator(request):
-    return render(request, 'generate_cv.html', {'page_title' : 'Generate your CV'})
+    edu = ['template1_education.html', 'template1_education.html', 'template1_education.html']
+    return render(request, 'generate_cv.html', {'page_title' : 'Generate your CV', 'edu' : edu})
