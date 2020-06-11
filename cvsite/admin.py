4ea50fcbc98defe7e419a9ab281b96f7e52635ff
keunnema <keunne.baudoin@yahoo.fr>
2020-06-11 21:19:44 +0200

    Add Languages Templates
    Wir haben die Namen aktualisiert und die Liste auf die Formularen erstellt.
    Edit und Delete müssen noch implementiert werden

--- cvsite/admin.py
+++ cvsite/admin.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from cvsite.models import *
+
 # Register your models here.
 
-admin.site.register(Beruf)
+admin.site.register(Experience)
 admin.site.register(Cv)
 admin.site.register(Data)
-admin.site.register(Kenntnisse)
-admin.site.register(Sprachen)
-admin.site.register(Ausbildung)
+admin.site.register(Skill)
+admin.site.register(Language)
+admin.site.register(Education)
